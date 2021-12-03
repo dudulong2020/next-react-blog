fac61ac6f383966437332fe831e78decf5927bf4
dudulong2020 <2414784235@qq.com>
2021-12-03 19:44:08 +0800

    package

--- next.config.js
+++ next.config.js
@@ -14,22 +14,22 @@ if (typeof require !== 'undefined') {
   require.extensions['.less'] = file => {};
 }
 
-// const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === 'production';
 
 module.exports = withLess(
   WithCss({
     lessLoaderOptions: {
       modifyVars: {
-        // 'primary-color': '#1DA57A'
+        'primary-color': '#1DA57A'
       },
       javascriptEnabled: true
     },
 
     // next-antd-ssr这个名字是你github项目名称
-    // assetPrefix: prod ? '/next-react-blog' : '',
-    // publicRuntimeConfig: {
-    //   linkPrefix: prod ? '/next-react-blog' : ''
-    // }
+    assetPrefix: prod ? '/next-react-blog' : '',
+    publicRuntimeConfig: {
+      linkPrefix: prod ? '/next-react-blog' : ''
+    }
     // assetPrefix: prod ? '/blog' : '',
     // publicRuntimeConfig: {
     //   linkPrefix: prod ? '/blog' : ''
