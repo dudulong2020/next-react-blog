497ac22231fa76694119a29a89f51cf65ec17a1e
dudulong2020 <2414784235@qq.com>
2021-12-04 19:21:14 +0800

    update

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/detail":["static\u002Fchunks\u002Fpages\u002Fdetail.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdetail"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
